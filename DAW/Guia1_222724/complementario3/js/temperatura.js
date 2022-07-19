cce5b7236d1929f4fda47e161d3fe828f8de19c1
Danughh <109573892+Danughh@users.noreply.github.com>
2022-07-19 07:11:07 -0600

    Add files via upload

---
+++ DAW/Guia1_222724/complementario3/js/temperatura.js
@@ -0,0 +1,9 @@
+function init(){
+    var op = prompt('Ingrese la Temperatura en grados ° Celcius: ', '');
+
+    var operaciones = document.getElementById('result');
+
+    result.innerHTML = "<p class=\"conversion\">" + "La conversión de C° es de: " + ((parseFloat(op) * 1.8)+32 ) + "°F" + "</p>\n";
+}
+
+window.onload = init;
